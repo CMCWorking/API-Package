9f6b6b036a934469ff1dd0667b3baa2eaf7b4a30
Cam <github@lekienhoanh.dev>
2022-06-22 17:55:38 +0700

    update Category API

--- app/Http/Controllers/Api/Category/CategoryV1Controller.php
+++ app/Http/Controllers/Api/Category/CategoryV1Controller.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Api\Category\CategoryTransformer;
 use App\Http\Controllers\Controller;
 use App\Models\Category;
 use Dingo\Api\Routing\Helpers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class CategoryV1Controller extends Controller
 {
@@ -16,5 +18,53 @@ class CategoryV1Controller extends Controller
         $categories = Category::paginate(10);
 
         return $this->response->paginator($categories, new CategoryTransformer());
+    }
+
+    public function show($id)
+    {
+        $category = Category::find($id);
+
+        return $this->response->item($category, new CategoryTransformer());
+    }
+
+    public function store(Request $request)
+    {
+        $category = Category::create([
+            'name' => $request->name,
+            'slug' => Str::slug($request->name),
+            'description' => $request->description,
+            'image' => $request->image,
+            'parent_id' => $request->parent_id,
+            'status' => $request->status,
+            'keywords' => $request->keywords,
+        ]);
+
+        return $this->response->item($category, new CategoryTransformer());
+    }
+
+    public function update(Request $request, $id)
+    {
+        $category = Category::find($id);
+
+        $category->update([
+            'name' => $request->name,
+            'slug' => $request->slug,
+            'description' => $request->description,
+            'image' => $request->image,
+            'parent_id' => $request->parent_id,
+            'status' => $request->status,
+            'keywords' => $request->keywords,
+        ]);
+
+        return $this->response->item($category, new CategoryTransformer());
+    }
+
+    public function destroy($id)
+    {
+        $category = Category::find($id);
+
+        $category->delete();
+
+        return $this->response->noContent();
     }
 }
