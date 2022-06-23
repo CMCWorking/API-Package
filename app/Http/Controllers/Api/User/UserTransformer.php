33a2529b0913342436169d3000e9c2cdd1348f05
Cam <github@lekienhoanh.dev>
2022-06-23 10:37:49 +0700

    added User API

---
+++ app/Http/Controllers/Api/User/UserTransformer.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Http\Controllers\Api\User;
+
+use App\Models\User;
+use League\Fractal\TransformerAbstract;
+
+class UserTransformer extends TransformerAbstract
+{
+    public function transform(User $user)
+    {
+        return [
+            'name' => $user['name'],
+            'email' => $user['email'],
+        ];
+    }
+}
