34583400722792e45b5822eac50d5b08d7f381fc
Cam <github@lekienhoanh.dev>
2022-06-23 15:46:04 +0700

    update PaymentMethod's API

--- app/Http/Controllers/Api/PaymentMethod/PaymentMethodV1Controller.php
+++ app/Http/Controllers/Api/PaymentMethod/PaymentMethodV1Controller.php
@@ -3,61 +3,68 @@
 namespace App\Http\Controllers\Api\PaymentMethod;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\PaymentMethodAPIRequest;
 use App\Models\PaymentMethod;
 use Dingo\Api\Routing\Helpers;
-use Illuminate\Http\Request;
 
 class PaymentMethodV1Controller extends Controller
 {
     use Helpers;
 
-    public function __construct()
+    public function __construct(PaymentMethod $payment_method)
     {
         $this->middleware('auth:sanctum');
+        $this->payment_method = $payment_method;
     }
 
     public function index()
     {
-        $payment_methods = PaymentMethod::paginate(10);
+        $payment_methods = $this->payment_method->paginate(10);
 
         return $this->response->paginator($payment_methods, new PaymentMethodTransformer());
     }
 
     public function show($id)
     {
-        $payment_method = PaymentMethod::find($id);
+        $payment_method = $this->payment_method->find($id);
 
         return $this->response->item($payment_method, new PaymentMethodTransformer());
     }
 
-    public function store(Request $request)
+    public function store(PaymentMethodAPIRequest $request)
     {
-        $payment_method = PaymentMethod::create([
+        $payment_method = $this->payment_method->create([
             'name' => $request->name,
             'description' => $request->description,
         ]);
 
-        return $this->response->created();
+        return $this->response->array([
+            'message' => 'Payment Method created successfully.',
+        ]);
     }
 
-    public function update(Request $request, $id)
+    public function update(PaymentMethodAPIRequest $request, $id)
     {
-        $payment_method = PaymentMethod::find($id);
+        $payment_method = $this->payment_method->find($id);
 
         $payment_method->update([
             'name' => $request->name,
             'description' => $request->description,
         ]);
 
-        return $this->response->item($payment_method, new PaymentMethodTransformer());
+        return $this->response->array([
+            'message' => 'Payment Method updated successfully.',
+        ]);
     }
 
     public function destroy($id)
     {
-        $payment_method = PaymentMethod::find($id);
+        $payment_method = $this->payment_method->find($id);
 
         $payment_method->delete();
 
-        return $this->response->noContent();
+        return $this->response->array([
+            'message' => 'Payment Method deleted successfully.',
+        ]);
     }
 }
