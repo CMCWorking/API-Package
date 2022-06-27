4267c7ab335dd7ff1dabf86a065eea1849932acd
Cam <github@lekienhoanh.dev>
2022-06-27 11:44:02 +0700

    update Filterable Traits

--- app/Traits/Filterable.php
+++ app/Traits/Filterable.php
@@ -13,18 +13,29 @@ trait Filterable
         foreach ($params as $field => $value) {
             $method = 'filter' . Str::studly($field);
 
-            if ($value != '') {
-                if (method_exists($this, $method)) {
-                    $this->{$method}($query, $value);
-                } else {
-                    if (!empty($this->filterable) && is_array($this->filterable)) {
-                        if (in_array($field, $this->filterable)) {
-                            $query->where($this->table . '.' . $field, $value);
-                        } elseif (key_exists($field, $this->filterable)) {
-                            $query->where($this->table . '.' . $this->filterable[$field], $value);
-                        }
-                    }
-                }
+            if ($value === '') {
+                continue;
+            }
+
+            if (method_exists($this, $method)) {
+                $this->{$method}($query, $value);
+                continue;
+            }
+
+            if (empty($this->filterable) || !in_array($field, $this->filterable)) {
+                continue;
+            }
+
+            if (in_array($field, $this->filterable)) {
+                $query->where($this->table . '.' . $field, 'like', '%' . $value . '%');
+
+                continue;
+            }
+
+            if (key_exists($field, $this->filterable)) {
+                $query->where($this->table . '.' . $this->filterable[$field], 'like', '%' . $value . '%');
+
+                continue;
             }
         }
 
