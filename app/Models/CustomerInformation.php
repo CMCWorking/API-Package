66cc9f5e03a9b4b01fcb7dfa96889f0ccb814c65
Cam <github@lekienhoanh.dev>
2022-06-27 10:50:11 +0700

    added Filterable trait

--- app/Models/CustomerInformation.php
+++ app/Models/CustomerInformation.php
@@ -2,15 +2,20 @@
 
 namespace App\Models;
 
+use App\Traits\Filterable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class CustomerInformation extends Model
 {
     protected $guarded = [];
-    use HasFactory;
+    use HasFactory, Filterable;
 
     protected $table = 'customer_informations';
+    protected $filterable = [
+        'login_engine',
+        'promotion' => 'receive_promotion',
+    ];
 
     public function addresses()
     {
