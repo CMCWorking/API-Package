cc43aca28d84dcbd4692beaff68a28974f708d57
Cam <github@lekienhoanh.dev>
2022-06-27 13:46:51 +0700

    added user search function

--- app/Http/Controllers/Api/User/UserV1Controller.php
+++ app/Http/Controllers/Api/User/UserV1Controller.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\UserAPIRequest;
 use App\Models\User;
 use Dingo\Api\Routing\Helpers;
+use Illuminate\Http\Request;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 class UserV1Controller extends Controller
@@ -16,6 +17,7 @@ class UserV1Controller extends Controller
     public function __construct(User $user)
     {
         $this->user = $user;
+        $this->page = 10;
     }
 
     public function index()
@@ -85,5 +87,16 @@ class UserV1Controller extends Controller
         return $this->response->array([
             'message' => 'User deleted successfully',
         ]);
+    }
+
+    public function search(Request $request)
+    {
+        $user = $this->user->filter($request->all())->paginate($request->paginate ?? $this->page);
+
+        if (count($user) < 1) {
+            return $this->response->errorNotFound('User not found');
+        }
+
+        return $this->response->paginator($user, new UserTransformer());
     }
 }
