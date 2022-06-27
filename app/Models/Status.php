65dcafa7ad5b388954ed4d0f2c39291eed11e82c
Cam <github@lekienhoanh.dev>
2022-06-27 14:17:39 +0700

    update Status filter APi

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
+use App\Traits\Filterable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Status extends Model
 {
-    use HasFactory;
+    use HasFactory, Filterable;
+
+    protected $filterable = [
+        'type',
+    ];
 }
