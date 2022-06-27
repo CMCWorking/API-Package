c9d48472b03f7a3ebdeeab735ed3a24d128dedaa
Cam <github@lekienhoanh.dev>
2022-06-27 14:00:49 +0700

    added Category filter

--- app/Models/category.php
+++ app/Models/category.php
@@ -2,12 +2,21 @@
 
 namespace App\Models;
 
+use App\Traits\Filterable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    use HasFactory;
+    use HasFactory, Filterable;
 
     protected $guarded = [];
+
+    protected $filterable = [
+        'name',
+        'slug',
+        'keywords',
+        'parent_id',
+        'status',
+    ];
 }
